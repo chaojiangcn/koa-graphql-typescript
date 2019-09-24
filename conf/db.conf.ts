39e1f465e53a8c1664ff7fde5340fe45e170746d
unknown <chaojiangcn@outlook.com>
2019-09-24 22:47:35 +0800

    提交

---
+++ conf/db.conf.ts
@@ -0,0 +1,22 @@
+const HOST = '0.0.0.0'
+export const MySqlConf = {
+  host: HOST,
+  port: 3306,
+  username: 'root',
+  password: 'root',
+  database: 'Blog'
+}
+export const MongoConf = {
+  host: HOST,
+  port: 27017,
+  username: 'root',
+  password: 'mongo-pwd',
+  database: 'BlogLog'
+}
+
+export const RedisConf = {
+  host: HOST,
+  port: 6379,
+  username: 'root',
+  password: 'redis-pwd'
+}
